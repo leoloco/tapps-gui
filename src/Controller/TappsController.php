b4db7858696c7a2fbe52086c3c3f1c3eb39217a6
Unknown <leonard.lys@gmail.com>
2017-07-12 16:54:56 +0200

    fix

--- src/Controller/TappsController.php
+++ src/Controller/TappsController.php
@@ -161,7 +161,8 @@ class TappsController extends AppController
         $query = $tapps->find();
         $query->where(['Tapps.id' => (int)$this->request->getParam('pass.0')]);
         $query->select('user_id');
-        $this->Flash->error($query->user_id);
+        $user_id = $query->first();
+        $this->Flash->error($user_id['user_id']);
         if (in_array($this->request->getParam('action'), ['edit','delete']) && $user['type']==='appmanager' && $user['id']===(int)$this->request->getParam('user_id')){
             return true;
         }
