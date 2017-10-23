747821cbfb15c9a6058e6695c4931d4a0a9a6d7b
Unknown <leonard.lys@gmail.com>
2017-10-22 23:19:23 -0300

    Edit delete

--- webroot/daemon.php
+++ webroot/daemon.php
@@ -150,6 +150,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
                         array_push($data[1], ['id' => $app['tpid'],'cdn_uri' => $app['cdn_uri'],'cdn_login' => $app['cdn_login'],'cdn_password' => $app['cdn_password']]);
                     }
                 }
+                foreach ($remote_app_list as $app){
+                    if(!in_array($app['tpid'], $local_app_list)){
+                        array_push($data[2], ['id' => $app['tpid'],'cdn_uri' => $app['cdn_uri'],'cdn_login' => $app['cdn_login'],'cdn_password' => $app['cdn_password']]);
+                    }
+                }
                 if(count($data)===0){
                     //echo "<br>Up to date";
                     echo json_encode($data);
