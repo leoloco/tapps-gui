2ee5e2ca111d4c7585d19559762f826c04128010
Unknown <leonard.lys@gmail.com>
2017-10-25 20:44:16 -0300

    test

--- webroot/sync.php
+++ webroot/sync.php
@@ -36,7 +36,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $sth->execute();
         }
         while ($row = $sth->fetch()){
-            echo print_r($row);
             array_push($local_app_list, ['id' => $row['tpid'],'cdn_uri' => $row['cdn_uri'],'cdn_login' => $row['cdn_login'],'cdn_password' => $row['cdn_password']]);
         }
         echo print_r($local_app_list);
