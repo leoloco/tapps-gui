27ab9be79ac5a599afb84aa1fd45ca6be60727ef
Unknown <leonard.lys@gmail.com>
2017-10-25 15:00:20 -0300

    fix

--- webroot/sync.php
+++ webroot/sync.php
@@ -5,7 +5,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-echo "ok";
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         http_response_code(200);
         $foo = file_get_contents("php://input");
