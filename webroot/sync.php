65bf5724302088d206b8f8500e92cc2369dbb50f
Unknown <leonard.lys@gmail.com>
2017-10-25 18:37:26 -0300

    try

--- webroot/sync.php
+++ webroot/sync.php
@@ -8,11 +8,9 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         http_response_code(200);
         $foo = file_get_contents("php://input");
-        $data = var_dump(json_decode($foo, true));
-        $out = array_values($data);
-        //echo print_r($data);
+        $data = json_decode($foo, true);
         header('Content-type: application/json');
-        echo json_encode($out);
+        echo json_encode($data);
 }    
 
 ?>
