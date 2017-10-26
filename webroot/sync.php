33d19e76a920fc589cb86d2ae1a506c4551c3501
Unknown <leonard.lys@gmail.com>
2017-10-26 13:50:43 -0300

    test

--- webroot/sync.php
+++ webroot/sync.php
@@ -45,11 +45,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             }
         }
         $remote_app_list=$remote_app_list['apps'];
-        echo print_r($remote_app_list);
-        echo print_r($local_app_list);
+        echo "remote : \n".print_r($remote_app_list);
+        echo "local : \n".print_r($local_app_list);
         foreach ($local_app_list as $app){
-            if(!in_array($app, $remote_app_list['apps'])){
-                //echo print_r($app);
+            if(!in_array($app, $remote_app_list)){
+                echo "result : \n".print_r($app);
             }
         }
         header('Content-type: application/json');
