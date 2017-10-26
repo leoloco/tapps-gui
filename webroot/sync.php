aa6a42b6e031017e670a148054b36ad03421a16f
Unknown <leonard.lys@gmail.com>
2017-10-25 21:11:05 -0300

    test

--- webroot/sync.php
+++ webroot/sync.php
@@ -18,19 +18,28 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         //Decoding JSON string to PHP array
         $remote_app_list = json_decode($json, true);
         
+        //Connecting to db
         $pdo = new PDO('mysql:host=localhost;dbname=tapps_db;charset=utf8mb4', 'root', 'data123$');
+        //Building querry
         $sth = $pdo->prepare('SELECT * FROM ownerships WHERE device_id = :device_id');
+        //Editing parameters
         $sth->bindParam(':device_id', $remote_app_list['device_id'], PDO::PARAM_INT);
-        $sth->execute();
-        
-        
+        //X ecute query
+        $sth->execute();  
+        //Putting results in array
         while ($row = $sth->fetch()){
             array_push($local_data, $row['tapp_id']);
         }
+        
+        //Getting full data of the app
         foreach ($local_data as $tpid){
+            //Building querry
             $sth = $pdo->prepare('SELECT tpid, cdn_uri, cdn_login, cdn_password FROM tapps WHERE id = :tpid');
+            //Editing parameters
             $sth->bindParam(':tpid', $tpid, PDO::PARAM_INT);
+            //X ecute query
             $sth->execute();
+            //Putting results in array
             while ($row = $sth->fetch()){
                 array_push($local_app_list, ['id' => $row['tpid'],'cdn_uri' => $row['cdn_uri'],'cdn_login' => $row['cdn_login'],'cdn_password' => $row['cdn_password']]);
             }
@@ -38,6 +47,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         
         header('Content-type: application/json');
         echo json_encode($local_app_list);
+        echo json_encode($remote_app_list);
 }    
 
 ?>
