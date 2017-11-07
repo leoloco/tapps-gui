1e04371124b829fc8fe80ce0ba471a4b130fcebb
Unknown <leonard.lys@gmail.com>
2017-11-07 01:31:16 -0300

    fix

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -241,8 +241,7 @@ class AppController extends Controller
                     foreach ($query as $tapp){
                         array_push($appList, $tapp['tapps_id']);
                     }
-                    if(!in_array($appList, $elements['id']))
-                    {
+                    if(!in_array($elements['id'],$appList)){
                         $queryTapps = $tapps->query();
                         $queryTapps->insert(['tapp_id','user_id'])
                                 ->values([
